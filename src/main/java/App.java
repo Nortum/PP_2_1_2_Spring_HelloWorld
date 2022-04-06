b24ca208bd300b1df83016246666d64d039ceafc
Nortum <varevtsev@yandex.ru>
2022-04-06 20:45:41 +0400

    Solution1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1);
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2);
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
     }
 }
